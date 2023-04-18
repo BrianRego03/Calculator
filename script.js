b805b9f700d36b17f33e5305136e312eae2792cd
Brian rego <brian.rego03@gmail.com>
2023-04-18 12:38:54 +0530

    Create functions for operations

---
+++ script.js
@@ -0,0 +1,15 @@
+function addition(num1,num2){
+    return num1+num2;
+}
+function subtraction(num1,num2){
+    return num1-num2;
+}
+function product(num1,num2){
+    return num1*num2;
+}
+function divide(num1,num2){
+    return num1/num2;
+}
+function remainder(num1,num2){
+    return num1%num2;
+}
