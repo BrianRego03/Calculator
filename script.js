8a337496757abd7f0efb7f4d5260f1a59f03f0fb
Brian rego <brian.rego03@gmail.com>
2023-04-18 13:00:42 +0530

    Create operate function to accept input

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let input1;
+let input2;
+let operator;
+
+
 function addition(num1,num2){
     return num1+num2;
 }
@@ -12,4 +17,23 @@ function divide(num1,num2){
 }
 function remainder(num1,num2){
     return num1%num2;
+}
+
+function operate(firstNum,secondNum,operation){
+    if(operation==='+'){
+        input1=addition(+firstNum,+secondNum);
+    }
+    else if(operation==='-'){
+        input1=subtraction(+firstNum,+secondNum);
+    }
+    else if(operation==='*'){
+        input1=product(+firstNum,+secondNum);
+    }
+    else if(operation==='/'){
+        input1=divide(+firstNum,+secondNum);
+    }
+    else if(operation==='%'){
+        input1=remainder(+firstNum,+secondNum);
+    }
+    return input1;
 }
